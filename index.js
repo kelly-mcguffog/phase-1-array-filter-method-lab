64451f57b8d26ad1abe44c872db47bc0f7550b3d
Kelly McGuffog <kellymcguffog@gmail.com>
2022-06-20 14:06:48 -0400

    commit tree 131893846e92f5042f99a95fba4d8e6a1fe69de1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string){
+    const result = drivers.filter(driver => driver.toUpperCase() === string.toUpperCase());
+    return result;
+}
+
+function fuzzyMatch(names, string){
+    const matchingLetters = names.filter(name => name.charAt(0) === string.charAt(0));
+    return matchingLetters;
+}
+
+function matchName(driver, string){
+    const matchingNames = driver.filter(driverName => driverName.name === string);
+    return matchingNames;
+}
